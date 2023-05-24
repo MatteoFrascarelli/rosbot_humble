d7fe663edd53a7dd56e53185af935791b41c5085
Massimo Teppati Losè <massimo.teppatilose@santannapisa.it>
2023-05-24 18:54:18 +0200

    another fix for vcs

--- clone_repos.sh
+++ clone_repos.sh
@@ -5,3 +5,5 @@ vcs import ros2_ws/ws/src < rosbot_origin.repos
 vcs import ros2_ws/ws/src < ros2_ws/ws/src/rosbot_xl_ros/rosbot_xl/rosbot_xl_hardware.repos
 
 vcs import ros2_ws/ws/src < ros2_ws/ws/src/rosbot_xl_ros/rosbot_xl/rosbot_xl_simulation.repos
+
+vcs import my_ros2_ws/src < ros2_learning.repos
