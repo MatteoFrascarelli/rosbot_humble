a44756003248df75dd7a5c9863673e331b27b978
campitellipiero <p.campitelli@studenti.unipi.it>
2023-07-13 19:11:45 +0200

    Upload new Piero's repository

--- my_ros2_ws/src/rosbot_object_recognition/setup.py
+++ my_ros2_ws/src/rosbot_object_recognition/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "obj_rec = rosbot_object_recognition.object_recognition.object_recognition:main"
+            "obj_rec = rosbot_object_recognition.object_recognition:main"
         
         ],
     },
